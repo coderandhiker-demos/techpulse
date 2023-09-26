8da63855ff288ad540687dc30cab5605a5a5c6b9
Chris Roberts <Chrisrt@microsoft.com>
2023-09-26 10:56:45 -0700

    added dev tag

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
-#docker run --name crawler-container techpulse-crawler
-docker run --name web-api-container -p 60010:60010 techpulse-web-api
+#docker run --name crawler-container techpulse-crawler:dev
+docker run --name web-api-container -p 60010:60010 techpulse-web-api:dev
