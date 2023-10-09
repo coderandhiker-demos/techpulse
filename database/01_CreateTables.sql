1e65f9e82a696cb3dcfce27c3c156b2122c0b43c
Chris Roberts <Chrisrt@microsoft.com>
2023-10-09 21:30:34 +0000

    Create database script(s) to main

---
+++ database/01_CreateTables.sql
@@ -0,0 +1,41 @@
+SET search_path = techpulse, public;
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    aad_object_id UUID, -- Store AAD Object ID for each user
+    date_added TIMESTAMP DEFAULT NOW(),    
+    email VARCHAR(255),
+    display_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS  rss_feeds (
+    feed_id SERIAL PRIMARY KEY,
+    url TEXT NOT NULL,
+    name VARCHAR(255),
+    date_added TIMESTAMP DEFAULT NOW(),
+    user_id INT REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS  articles (
+    entry_id SERIAL PRIMARY KEY,
+    title TEXT,
+    link TEXT,
+    published TIMESTAMP,
+    content TEXT,
+    feed_id INT REFERENCES rss_feeds(feed_id)
+);
+
+CREATE TABLE IF NOT EXISTS  feed_favorites (
+    favorite_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    feed_id INT REFERENCES rss_feeds(feed_id),
+    favorite_date TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS  article_favorites (
+    favorite_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    entry_id INT REFERENCES articles(entry_id),
+    favorite_date TIMESTAMP DEFAULT NOW()
+);
--- database/install-database.sh
+++ database/install-database.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+# If the service is offline, run this as sudo
+#service postgresql restart
+
 ERRORS=""
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NO_COLOR='\033[0m'
 
-# Check for PG_PYTHON_DBCONNECTION environment variable
-if [ -z "${PG_PYTHON_DBCONNECTION}" ]; then
-    ERRORS="${ERRORS}${RED}Error: PG_PYTHON_DBCONNECTION is not set.${NO_COLOR}"
-    ERRORS="${ERRORS}${YELLOW}\nPlease set it using the following command:"
-    ERRORS="${ERRORS}\nexport PG_PYTHON_DBCONNECTION='your_connection_string_here'${NO_COLOR}"
-fi
+# PostgreSQL connection parameters
+DB_HOST="127.0.0.1"
+DB_PORT="5432"
+DB_USERNAME="postgres"
+DB_NAME="techpulse"
 
-# Add extra newline if ERRORS is already set
-if [ ! -z "$ERRORS" ]; then
-    ERRORS="${ERRORS}\n"
-fi
+# Define the PostgreSQL connection string
+PG_PYTHON_DBCONNECTION="postgresql://$DB_USERNAME@$DB_HOST:$DB_PORT/$DB_NAME"
 
 # Check for psql tool
 if ! command -v psql &> /dev/null; then
@@ -29,10 +29,14 @@ if [ ! -z "$ERRORS" ]; then
     exit 1
 fi
 
+# Create the database if it doesn't exist
+echo "Creating database $DB_NAME if it doesn't exist..."
+createdb $DB_NAME
+
 # Get a list of all .sql scripts in the current directory, sorted by their prefix number
 SQL_SCRIPTS=$(ls | grep -E '^[0-9]+_.*\.sql$' | sort -n)
 
-# Count scripts for final message
+# Count scripts for the final message
 NumScripts=$(echo "$SQL_SCRIPTS" | wc -l)
 
 # Loop through and execute each SQL script
@@ -46,4 +50,4 @@ for script in $SQL_SCRIPTS; do
 done
 
 # Display only the hostname and database name in the final message
-echo "$NumScripts scripts have been executed."
+echo "$NumScripts scripts have been executed in database $DB_NAME."
